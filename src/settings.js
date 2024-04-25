04a453de1c6cbed84586d7f2dde772d42103a997
DDLQAQ <2442914896@qq.com>
2024-04-25 21:54:49 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  // 网站标题
+  title: '人力资源管理--ddl',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true, // 是否固定头部
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false // 是否显示侧边栏logo
+}
